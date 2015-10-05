ef6a80e5d06af9e2db22d09a48f16d4638ee7d36
Darren166 <darren.hall@cognim.co.uk>
2015-10-05 18:33:07 +0100

    First passing test

---
+++ JustEat.Tests/ServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using JustEat.Services;
+using JustEat.Models;
+using FluentAssertions;
+
+namespace JustEat.Tests
+{
+	[TestFixture]
+	public class ServiceTests
+	{
+		[Test]
+		public void GetRestaurantsByOutcode_Returns_Correctly()
+		{
+			// Given an outcode of SE19
+			var outCode = "SE19";
+
+			// When we call the service method with that outcode
+			var restaurantService = new RestaurantService();
+			var actual = restaurantService.GetRestaurantsByOutcode(outCode);
+
+			// Then the restaurant list is as expected
+			var expected = new List<Restaurant> { 
+			new Restaurant{
+				Id = 1,
+				Name = "Test",
+				NumberOfRatings = 10,
+				RatingStars = 2.0,
+				CuisineTypes = new List<CuisineType> {
+					new CuisineType {
+						Id = 1,
+						Name = "Chinese"
+					}
+				}
+			}};
+			actual.ShouldBeEquivalentTo(expected);
+		}
+	}
+}

d1f4e5c1dbfa6f6b0bbd7ab70d6a895eda1a104d
Darren166 <darren.hall@cognim.co.uk>
2015-10-05 18:34:35 +0100

    Included missing files

---
+++ JustEat.Services/Models/CuisineType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustEat.Models
+{
+	public class CuisineType
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ JustEat.Services/RestaurantService.cs
@@ -0,0 +1,28 @@
+﻿using JustEat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustEat.Services
+{
+    public class RestaurantService
+    {
+		public List<Restaurant> GetRestaurantsByOutcode(string outcode) {
+			return new List<Restaurant> { 
+			new Restaurant{
+				Id = 1,
+				Name = "Test",
+				NumberOfRatings = 10,
+				RatingStars = 2.0,
+				CuisineTypes = new List<CuisineType> {
+					new CuisineType {
+						Id = 1,
+						Name = "Chinese"
+					}
+				}
+			}};
+		}
+    }
+}
